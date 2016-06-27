75dbfd3e5acfc572e20ed3913c28dfcdb65267d8
qwedos <qwedos@gmail.com>
2016-06-27 16:27:57 +0600

    Added 404 page template

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,29 @@
 @extends('layouts.frontend.template')
+
+@section('title')
+@endsection
+
+@section('search')
+    <section class="well_search bg1">
+        <div class="container">
+
+            <div class="h1 clr-black text-center">
+                404<br>
+                Страница которую вы ищете, не найдена<br>
+                Найдите то, что искали
+            </div>
+
+            {!! Form::open(array('url' => '', 'class' => 'search-form-all')) !!}
+            <label class="search-form_label">
+                {{Form::text('s', $first_name = null, array('class' => 'search-form_input', 'placeholder' => 'Компании,  Сервисы,  Банкоматы'))}}
+                <span class="search-form_liveout"></span>
+            </label>
+            {{Form::submit('Поиск', array('class' => 'search-form_submit btn btn-primary'))}}
+            {!! Form::close() !!}
+        </div>
+    </section>
+@endsection
+
+@section('content')
+
+@endsection
