2170da96b36271197575e711247ae518d08b3c8a
qwedos <qwedos@gmail.com>
2016-11-04 19:56:31 +0600

    Added techworks view

---
+++ resources/views/techworks.blade.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Infomob.kz</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('images/icons/favicon.ico') }}" type="image/x-icon">
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+</head>
+<body>
+    <div class="container" style="margin-top: 200px; font-size: 18px;">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3 text-center">
+                Ведутся технические работы.<br>
+                Приносим извинения за неудобства!
+            </div>
+        </div>
+    </div>
+</body>
+</html>
